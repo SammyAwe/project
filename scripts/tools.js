100c2bdbc24f23251618dddfaba731827ff4c99a
SammyAwe <awesamuelopeoluwa@gmail.com>
2024-05-31 23:01:11 -0700

    First project commit, html, css and minor js done, will fix more later

---
+++ scripts/tools.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const menuIcon = document.getElementById('menu-icon');
+    const nav = document.querySelector('nav');
+
+    menuIcon.addEventListener('click', function(){
+        if (nav.style.display === 'block') {
+            nav.style.display = 'none';
+        } else {
+            nav.style.display = 'block';
+        }
+    });
+});
